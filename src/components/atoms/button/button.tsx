77038e8fe420f710311e74d43f0bfc9b9c605658
Caro <carole.hug@outlook.com>
2023-01-20 22:06:36 +0100

    refactor buttons

--- src/components/atoms/button/button.tsx
+++ src/components/atoms/button/button.tsx
@@ -1,21 +1,14 @@
 import React, { FC, ReactNode } from 'react';
 
-export enum ButtonType {
-  primary = 'primary',
-  secondary = 'secondary'
-}
-
 export type ButtonProps = {
   size?: 'M' | 'L';
   label?: string;
-  classes?: string;
   variant?: 'default' | 'purple' | 'gradient';
-  test?: ButtonType;
   children?: ReactNode;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-const Button: FC<ButtonProps> = ({ size, label, classes, onClick, variant, children, test }) => {
+const Button: FC<ButtonProps> = ({ size, label, onClick, variant, children }) => {
   const purpleVariant =
     'bg-violet-600 hover:bg-violet-700 hover:outline-violet-100 active:outline-violet-200';
   const defaultVariant =
@@ -28,7 +21,7 @@ const Button: FC<ButtonProps> = ({ size, label, classes, onClick, variant, child
   const currentSize = size === 'M' ? 'w-150 h-40' : 'w-178 h-48';
   return (
     <button
-      className={`${defaultClasses} - ${classes} - ${currentSize} - ${
+      className={`${defaultClasses} - ${currentSize} - ${
         variant === 'purple'
           ? purpleVariant
           : variant === 'gradient'
