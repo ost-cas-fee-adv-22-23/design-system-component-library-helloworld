c5d065032e678afff98d34704a187d59e87af47a
Caro <carole.hug@outlook.com>
2023-03-09 15:10:57 +0100

    update component exports

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,8 +1,12 @@
 export { default as IconButton } from './icon-button';
 export { default as Button } from './button';
+export { default as CommentButton } from './comment-button';
+export { default as CopyButton } from './copy-button';
+export { default as LikeButtonWithReactionButton } from './like-button-with-reaction';
+export { default as InteractionButton } from './interaction-button';
 export { default as Label } from './label';
 export { default as ProfilePic } from './profile-pic';
 export { default as Link } from './link';
 export { default as Textfield } from './textfield';
 export * from './icons';
-export * from './branding/branding-logo';
+export * from './branding';
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export { default as Input } from './input';
 export { default as Card } from './card';
+export { default as ProfileHeader } from './profile-header';
