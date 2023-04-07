5ae160f7898cc4238178f70ebd54305f670c7546
Mehmet Ali Bekooglu <mehmetali.bekooglu@ost.ch>
2023-04-07 22:42:13 +0200

    feat: Changed user profile by removing background image

--- src/components/organisms/user-profile/user-profile.stories.tsx
+++ src/components/organisms/user-profile/user-profile.stories.tsx
@@ -1,6 +1,6 @@
 import {ComponentMeta, ComponentStory} from "@storybook/react";
 import UserProfile from "./user-profile";
-import {ProfileHeaderLabelType, ProfileHeaderPictureSize} from "../../molecules/profile-header/profile-header";
+import {ProfileHeaderLabelType, ProfileHeaderPictureSize} from "../../molecules";
 import React from "react";
 
 export default {
@@ -9,7 +9,10 @@ export default {
 } as ComponentMeta<typeof UserProfile>
 
 const Template: ComponentStory<typeof UserProfile> = (args) => (
-    <UserProfile {...args} />
+    <div>
+        <img alt={'image'} className={'object-cover rounded-m w-full h-full'} src={'https://picsum.photos/600/400'}/>
+        <UserProfile {...args} />
+    </div>
 );
 
 export const defaultUserProfile = Template.bind({});
@@ -24,7 +27,6 @@ defaultUserProfile.args = {
     imageSrc: 'https://media.licdn.com/dms/image/D4E03AQEXHsHgH4BwJg/profile-displayphoto-shrink_800_800/0/1666815812197?e=2147483647&v=beta&t=Vx6xecdYFjUt3UTCmKdh2U-iHvY0bS-fcxlp_LKbxYw',
     timestamp: 'vor 42 Minuten',
     username: 'robertvogt',
-    img: 'https://picsum.photos/800/600',
     children: 'Quia aut et aut. Sunt et eligendi similique enim qui quo minus. Aut aut error velit voluptatum optio sed quis cumque error magni.',
     href: 'https://www.google.com/'
 };
