6b61f948cc34b1cbdc31dae15d638c9628b980d1
Mehmet Ali Bekooglu <mehmetali.bekooglu@ost.ch>
2023-04-18 14:08:37 +0200

    fix: Button for mobile view

--- src/components/molecules/profile-header/profile-header.tsx
+++ src/components/molecules/profile-header/profile-header.tsx
@@ -67,7 +67,7 @@ const ProfileHeader: FC<ProfileHeaderProps> = ({
             </div>
             <div>
                 <NextLink href={href} linkComponent={link}>{labelVariantStyles[labelType]}</NextLink>
-                <div className={'flex'}>
+                <div className={'flex flex-row'}>
                     {username && hrefProfile && (
                         <NextLink href={href} linkComponent={link}><Label label={username} mainTyp={true} variant={'username'} onClick={onLabelClick}></Label></NextLink>
                     )}
